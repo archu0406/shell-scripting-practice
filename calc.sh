3b8fff1b58c72d2b922ed75dc36f7ea8c5e70cb2
archana vinayagam <saiarchana.93@gmail.com>
2021-11-07 11:51:58 +0530

    executing operation

--- calc.sh
+++ calc.sh
@@ -16,5 +16,32 @@ echo " Welcome to simple interative calculator"
  echo " Enter the option you need to perform:"
         read OPERATION
 # If the selection matches a supported operation, execute the operation.
+        case $OPERATION in
+                "add")
+
+                        let "total=$1+$2"
+                        echo "The addition of numbers is: $total"
+                        ;;
+                "subtract"|"s")
+
+                        let "total=$1-$2"
+                        echo "The subtraction of numbers is:$total"
+                        ;;
+                "multiply")
+
+                        let "total=$1*$2"
+                        echo "The multiplication of numbers is:$total"
+                        ;;
+                "divide")
+
+                        let "total=$1/$2"
+                        echo "The division of numbers is:$total"
+                        ;;
+                "exit")
+                        echo "Thank you for performing the operations!!"
+                        break
+                        ;;
+        esac
+
 # If the selection does not match a support operation, display an error message.
 # When the operation is complete, redisplay the menu.
