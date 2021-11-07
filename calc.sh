d63c117426424df6f8e72b20476ef71fae5ea4b0
archana vinayagam <saiarchana.93@gmail.com>
2021-11-07 11:50:01 +0530

    capturing user selection

--- calc.sh
+++ calc.sh
@@ -13,6 +13,8 @@ echo " Welcome to simple interative calculator"
         4) Divide
         5) Exit"
 # Then, capture the user selection.
+ echo " Enter the option you need to perform:"
+        read OPERATION
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
 # When the operation is complete, redisplay the menu.
